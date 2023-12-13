c26fc39e01b2579327132d975b2b067cc6ceb194
Enkovich <duma01.ru@mail.ru>
2023-12-13 12:30:41 +0300

    дз10 фикс

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-testVar = input("Введите число: ")
+setting = {
+    'numAttempt': 8,
+    'maxNum': 9,
+    'numInts': 4
+}
 
-print(type(testVar))
+# functions
+def helpCommand():
+    print('''help - справка по командам
+currSetting - текущие настройки для игры
+exit - выход из игры''')
+def currComSetting():
+    print(f'''Кол-во попыток: {setting['numAttempt']}
+Диапазон генерируемых чисел: 0 - {setting['maxNum']}
+Кол-во генерируемых чисел: {setting['numInts']}''')
+def editSettings():
+    try:
+        tmp = input()
+    except Exception as e:
+        print(e)
+
+def main():
+    while True:
+        command = input()
+        if command=='exit':
+            break
+        elif command=='help':
+            helpCommand()
+        elif command=='currSetting':
+            currComSetting()
+        elif command=='editSetting':
+            editSettings()
+
+# code
+
+main()
